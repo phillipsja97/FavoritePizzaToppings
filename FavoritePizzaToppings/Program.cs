174df7e76a9626d4d72e2908302fb86f0df56630
Jamie <phillipsja97@gmail.com>
2020-03-23 21:14:14 -0500

    finished the challenge

--- FavoritePizzaToppings/Program.cs
+++ FavoritePizzaToppings/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace FavoritePizzaToppings
 {
@@ -6,7 +10,32 @@ namespace FavoritePizzaToppings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(File.ReadAllText(@"c:./pizzas.json"));
+
+           var toppingLists = pizzas.Select(p => string.Join(",", p.Toppings.OrderBy(t => t)));
+
+           var distinctToppingCombinations = toppingLists.Distinct();
+
+           var countOfCombinations = new Dictionary<string, int>();
+
+            foreach (var combination in toppingLists)
+            {
+                if (!countOfCombinations.ContainsKey(combination))
+                {
+                    countOfCombinations.Add(combination, 1);
+                }
+                else
+                {
+                    countOfCombinations[combination] += 1;
+                }
+            }
+
+            var mostOrdered = countOfCombinations.OrderByDescending(item => item.Value).Take(20);
+            foreach (var (combination, count) in mostOrdered)
+            {
+                Console.WriteLine($"The topping combination of {combination} was ordered {count} times.");
+            }
+            Console.ReadLine();
         }
     }
 }
